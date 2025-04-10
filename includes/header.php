4224e7c7a78204ec2f54117f5399cef3f2d0ca5d
saiganesh <saiganesh123k@gmail.com>
2025-04-10 13:16:59 +0530

    changes updated

--- includes/header.php
+++ includes/header.php
@@ -30,6 +30,7 @@ $conn = $db->getConnection();
     <title>Simple CRUD Application</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo $path_adjust; ?>css/style.css">
 </head>
 <body>
